98441394f47c71192b04d37013fe59cb899b9c74
Cintia <Cintia@MXlinux23>
2025-03-04 12:33:09 -0300

    creando html, js y css

---
+++ asincronismo1/main.js
@@ -0,0 +1,6 @@
+let conteinerPpal = document.getElementById('conteinerPpal');
+let divImage = document.getElementById('divImage');
+let divInfo = document.getElementById('divInfo');
+let imgToday = document.getElementById('imgToday');
+
+
