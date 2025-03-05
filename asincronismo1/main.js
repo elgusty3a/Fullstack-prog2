0972bfad99ca4b0550c4f1ca33a8f0dd76c3c053
Gustavo Arias <ariasgustavo3a@gmail.com>
2025-03-05 00:25:42 -0300

    se logro consumir una API de superheroes

--- asincronismo1/main.js
+++ asincronismo1/main.js
@@ -1,6 +1,86 @@
+//4cf50017dbf46f5b4ae8e9c41a89bded token para https://superheroapi.com/
+
 let conteinerPpal = document.getElementById('conteinerPpal');
-let divImage = document.getElementById('divImage');
-let divInfo = document.getElementById('divInfo');
-let imgToday = document.getElementById('imgToday');
+let cards = document.getElementById('cards');
+// let divImage = document.getElementById('divImage');
+// let divInfo = document.getElementById('divInfo');
+
+let inputName = document.getElementById('nameCharacter');
+let searchByName = document.getElementById('btnName');
+let inputId = document.getElementById('idCharacter');
+let searchById = document.getElementById('btnId');
+
+searchByName.addEventListener("click", searchNameAPI);
+searchById.addEventListener("click", searchIdApi);
+
+async function searchIdApi() {
+  try {
+    let id = inputId.value;
+    // console.log(id);
+    if (id !== ""){
+      let idToSearch = await fetch(`https://www.superheroapi.com/api.php/4cf50017dbf46f5b4ae8e9c41a89bded/${id}`);
+      let data = await idToSearch.json();
+      // console.log(data);
+      inputId.value = "";
+      printDataId(data);
+      // return data;
+    }
+  } catch (error) {
+    console.log("Error:" + error);
+  }
+};
+
+async function searchNameAPI() {
+  try {
+    let nameHeroe = inputName.value;
+    // console.log(nameHeroe);
+    if (nameHeroe !== ""){
+      let idToSearch = await fetch(`https://www.superheroapi.com/api.php/4cf50017dbf46f5b4ae8e9c41a89bded/search/${nameHeroe}`);
+      let data = await idToSearch.json();
+      console.log(data);
+      inputName.value = "";
+      printDataName(data);
+      // return data;
+    }
+  } catch (error) {
+    console.log("Error:" + error);
+  }
+};
+
+function printDataName(data) {
+  data.results.forEach(version => {
+    printDataId(version);
+  });
+}
+
+
+function printDataId(data) {
+  console.log(data);
+  let divImage = document.createElement("div");
+  cards.appendChild(divImage);
+  let divInfo = document.createElement("div");
+  cards.appendChild(divInfo);
+  let heroeImg = document.createElement("img");
+  heroeImg.src = data.image.url;
+  divImage.appendChild(heroeImg);
+  let heroeName = document.createElement("h1");
+  heroeName.textContent = data.name;
+  divInfo.appendChild(heroeName);
+  let heroeAlignment = document.createElement("h3");
+  heroeAlignment.textContent = data.biography.alignment;
+  divInfo.appendChild(heroeAlignment);
+  let heroePowerstats = document.createElement("ul");
+  divInfo.appendChild(heroePowerstats);
+  let powerstats = data.powerstats;
+  console.log(powerstats);
+  for (let power in powerstats) {
+    let item = document.createElement("li");
+    item.textContent = `${power}: ${powerstats[power]}`;
+    heroePowerstats.appendChild(item);
+  }
+}
+
+
+
 
 
