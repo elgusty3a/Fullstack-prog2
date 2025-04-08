a4048acf663c868958e24358472b86521e5399f1
Arias Gustavo <ariasgustavo3a@gmail.com>
2025-04-08 08:02:34 -0300

    creacion de metodos en gestorLibros

--- POO/ej-biblioteca/gestorLibros.ts
+++ POO/ej-biblioteca/gestorLibros.ts
@@ -5,23 +5,48 @@ import * as rdl from 'readline-sync';
 
 export class GestorLibros {
 
+  private nombre:string;
   private libros:Libro[];
-  private socio:Socio[];
+  private socios:Socio[];
 
-  constructor() {
+  constructor(pNombre:string) {
+    this.nombre = pNombre;
+    this.libros = new Array();
+    this.socios = new Array();
+  }
+
+  public crearLibro():Libro{
+
+  }
+
+  public insertarLibro(pLibro:Libro){
+    this.libros.push(pLibro);
+  }
+
+  public insertarLibros(pLibro:Libro[]){
+    pLibro.forEach(libro => {
+      this.insertarLibro(libro);
+    });
+  }
+
+  public existeLibro(pTitulo:string):number{
+    let indice:number = this.libros.findIndex(libro => libro.getTitulo() == pTitulo);
+    return indice;
+  }
+
+  public imprimirLibro(pIndice:number){
+    console.log(this.libros[pIndice]);
+  }
+
+  public modificar(pTitulo:string){
+    if (this.existeLibro(pTitulo)!=undefined) {
+      let indice:number=this.existeLibro(pTitulo);
+      this.menuModificar(indice);
+    }
+  }
+
+  private menuModificar(pIndice:number){
     
-  }
-
-  public insertar(){
-    
-  }
-
-  public consultar(){
-
-  }
-
-  public modificar(){
-
   }
 
   public eliminar(){
--- POO/ej-biblioteca/index.ts
+++ POO/ej-biblioteca/index.ts
@@ -3,3 +3,5 @@ import * as rdl from 'readline-sync';
 import { Socio } from "./socio";
 import { GestorLibros } from "./gestorLibros";
 import { Libro } from "./libro";
+
+let sarmiento = new GestorLibros("Sarmiento");
